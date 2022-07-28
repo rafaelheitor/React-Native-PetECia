2a3bb4725a1a3d13735de189731c15bdef46e804
Rafael Heitor <rafaelheitor2@outlook.com>
2022-07-28 15:14:29 -0300

    Organizing files

--- App.tsx
+++ App.tsx
@@ -1,47 +1,5 @@
-import { StatusBar } from "expo-status-bar"
-import { useState } from "react"
-import { StyleSheet, Text, View } from "react-native"
-import { TextInputElement } from "./src/components/TextInputElement"
+import { Home } from "./src/Views/Home"
 
 export default function App() {
-  const [inputText, setInputText] = useState("")
-  console.log(inputText)
-
-  function handleChange(text: string) {
-    setInputText(text)
-  }
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <Text style={styles.text}>Meu primeiro aplicativo</Text>
-        <View style={styles.input}>
-          <TextInputElement text={inputText} handleChange={handleChange} />
-        </View>
-      </View>
-
-      <StatusBar style='auto' />
-    </View>
-  )
+  return <Home />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#A52A2A",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  inputContainer: {
-    padding: 50,
-    paddingTop: 300,
-  },
-  input: {
-    paddingTop: 20,
-  },
-
-  text: {
-    fontSize: 20,
-    color: "#fff",
-  },
-})
---
+++ src/Views/Home/index.tsx
@@ -0,0 +1,47 @@
+import { StatusBar } from "expo-status-bar"
+import { useState } from "react"
+import { StyleSheet, Text, View } from "react-native"
+import { TextInputElement } from "../../components/TextInputElement"
+
+export const Home = () => {
+  const [inputText, setInputText] = useState("")
+  console.log(inputText)
+
+  function handleChange(text: string) {
+    setInputText(text)
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <Text style={styles.text}>Meu primeiro aplicativo</Text>
+        <View style={styles.input}>
+          <TextInputElement text={inputText} handleChange={handleChange} />
+        </View>
+      </View>
+
+      <StatusBar style='auto' />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#A52A2A",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputContainer: {
+    padding: 50,
+    paddingTop: 300,
+  },
+  input: {
+    paddingTop: 20,
+  },
+
+  text: {
+    fontSize: 20,
+    color: "#fff",
+  },
+})
